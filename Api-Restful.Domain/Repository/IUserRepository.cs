b4f0b99e5844175bbd3653f01610df36de93b501
cesc88 <cesc_peace@hotmail.com>
2020-10-06 21:20:34 -0300

    Creating interface IUserRepository

---
+++ Api-Restful.Domain/Repository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Api_Restful.Domain.Entities;
+using Api_Restful.Domain.Interfaces;
+
+namespace Domain.Repository
+{
+    public interface IUserRepository : IRepository<UserEntity>
+    {
+        Task<UserEntity> FindByLogin(string email);
+    }
+}
