584740669aa9df3a04338b435c1e114a56d320f8
cesc88 <cesc_peace@hotmail.com>
2020-10-07 08:44:41 -0300

    Implementing interface ILoginService'

---
+++ Api-Restful.Domain/Interfaces/Services/ILoginService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Api_Restful.Domain.Entities;
+
+namespace Domain.Interfaces.Services
+{
+    public interface ILoginService
+    {
+        Task<object> FindByLogin(UserEntity user);
+    }
+}
