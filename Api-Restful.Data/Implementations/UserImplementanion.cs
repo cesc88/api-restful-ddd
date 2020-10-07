1021df1943efcf7f5630bb113a700957835095d9
cesc88 <cesc_peace@hotmail.com>
2020-10-06 21:53:27 -0300

    Creting class UserImplementation

--- Api-Restful.Data/Implementations/UserImplementanion.cs
+++ Api-Restful.Data/Implementations/UserImplementanion.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Api_Restful.Data.Context;
+using Api_Restful.Data.Repository;
+using Api_Restful.Domain.Entities;
+using Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+
 namespace Data.Implementations
 {
-    public class UserImplementanion
+    public class UserImplementanion : BaseRepository<UserEntity>, IUserRepository
     {
-        public UserImplementanion()
+        private readonly DbSet<UserEntity> _dataset;
+
+        public UserImplementanion(MyContext context) : base(context)
         {
+            _dataset = context.Set<UserEntity>();
+        }
+
+        public async Task<UserEntity> FindByLogin(string email)
+        {
+            return await _dataset.FirstOrDefaultAsync(x => x.Email.Equals(email));
         }
     }
 }
