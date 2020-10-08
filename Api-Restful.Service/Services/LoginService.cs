3fc28754b00fc3cbc45525285fa7389ffcfd723d
cesc88 <cesc_peace@hotmail.com>
2020-10-08 20:39:50 -0300

    Implementing service LoginService

---
+++ Api-Restful.Service/Services/LoginService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Api_Restful.Domain.Entities;
+using Domain.Interfaces.Services;
+using Domain.Repository;
+
+namespace Service.Services
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IUserRepository _repository;
+
+        public LoginService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<object> FindByLogin(UserEntity user)
+        {
+            if (user != null && !string.IsNullOrWhiteSpace(user.Email))
+            {
+                return await _repository.FindByLogin(user.Email);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
