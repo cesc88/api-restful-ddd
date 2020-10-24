c305f1b70d1be09556cee14cb7d805d5f7172857
cesc88 <cesc_peace@hotmail.com>
2020-10-23 21:40:04 -0300

    Update UserDto

--- Api-Restful.Domain/Dtos/User/UserDto.cs
+++ Api-Restful.Domain/Dtos/User/UserDto.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain.Dtos.User
 {
     public class UserDto
     {
-        public UserDto()
-        {
-        }
+        
+        [Required(ErrorMessage = "")]
+        [StringLength(60, ErrorMessage = "")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "")]
+        [EmailAddress(ErrorMessage = "")]
+        [StringLength(100, ErrorMessage = "")]
+        public string Email { get; set; }
     }
 }
